ba5e2191e045bf9a3a3978b31b4586e658b555f3
afeiship <1290657123@qq.com>
2024-04-20 12:03:14 +0800

    feat: add empty-state

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <>
+      <div className='box'>
       <EmptyState title="404">
         <p>
           11,535 inspirational designs, illustrations, and graphic elements from the world’s best
@@ -12,6 +13,15 @@ function App() {
         </p>
         <button>Try Again</button>
       </EmptyState>
+      </div>
+
+      <div className='box'>
+      <EmptyState style={{'--centered': '50%'}} icon={<img width={120} src="https://tva1.js.work/large/007S8ZIlgy1gexw87htqhj305k05k74o.jpg" />}>
+        <p className='is-title'>
+         程序员紧张开发中，敬请期待！
+        </p>
+      </EmptyState>
+      </div>
     </>
   );
 }
